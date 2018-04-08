e34309d7ac23295b46559bcdc339fa26c47c3ecd
Timothy Brewster <trbrwstr@protonmail.com>
2018-04-08 12:25:22 -0700

    added dependencies/added varabiles in liri.js/started readme

--- liri.js
+++ liri.js
@@ -1,1 +1,9 @@
 require("dotenv").config();
+var fs = require("fs");
+var keys = require("./keys.js");
+var inqurier = require("inquirer");
+var twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
